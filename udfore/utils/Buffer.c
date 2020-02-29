2e6fd8c95ec0a522c234d50f33e6779ff7821a2b
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-02-29 11:31:04 +0100

    Utils: Added buffer builder.

---
+++ udfore/utils/Buffer.c
@@ -0,0 +1,52 @@
+#include "udfore/utils/Buffer.h"
+
+Buffer *buffer_create(size_t preallocated)
+{
+    Buffer *buffer = __create(Buffer);
+
+    buffer->buffer = calloc(preallocated, sizeof(char));
+    buffer->size = preallocated;
+    buffer->used = 0;
+
+    return buffer;
+}
+
+void buffer_destroy(Buffer *buffer)
+{
+    if (buffer->buffer)
+    {
+        free(buffer->buffer);
+    }
+
+    free(buffer);
+}
+
+const char *buffer_finalize(Buffer *buffer)
+{
+    char *result = buffer->buffer;
+    buffer->buffer = NULL;
+
+    buffer_destroy(buffer);
+
+    return result;
+}
+
+void buffer_append_str(Buffer *buffer, const char *str)
+{
+    for (size_t i = 0; str[i]; i++)
+    {
+        buffer_append_chr(buffer, str[i]);
+    }
+}
+
+void buffer_append_chr(Buffer *buffer, char chr)
+{
+    if (buffer->used + 1 == buffer->size)
+    {
+        buffer->size *= 1.25;
+        buffer->buffer = realloc(buffer->buffer, buffer->size);
+    }
+
+    buffer->buffer[buffer->used] = chr;
+    buffer->used++;
+}
---
+++ udfore/utils/Buffer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "udfore/utils/Utils.h"
+
+typedef struct
+{
+    size_t used;
+    size_t size;
+    char *buffer;
+} Buffer;
+
+Buffer *buffer_create(size_t preallocated);
+
+void buffer_destroy(Buffer *buffer);
+
+const char *buffer_finalize(Buffer *buffer);
+
+void buffer_append_str(Buffer *buffer, const char *str);
+
+void buffer_append_chr(Buffer *buffer, char chr);
